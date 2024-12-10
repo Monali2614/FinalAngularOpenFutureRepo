bcd9298bf35d1b31399671c4c21fd86a50588161
Monali2614 <112397573+Monali2614@users.noreply.github.com>
2024-12-10 16:16:48 +0530

    final repo

---
+++ src/app/testing/testing.component.ts
@@ -0,0 +1,36 @@
+import { Component, AfterViewInit } from '@angular/core';
+import SwiperCore, { Pagination } from 'swiper';
+// Swiper modules ko register karna
+SwiperCore.use([Pagination]);
+
+@Component({
+  selector: 'app-testing',
+  templateUrl: './testing.component.html',
+  styleUrls: ['./testing.component.css'],
+})
+export class TestingComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const swiper = new SwiperCore('.mySwiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: (index, className) => {
+          return '<span class="' + className + '" >' + ("⦿") + '</span>';
+        },
+      },
+
+      slidesPerView: 1,
+      spaceBetween: 20,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+}
+
+
